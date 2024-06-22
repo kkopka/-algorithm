c78b8a5726e2d041a1719f09527b5f569a17e341
SBushmelev <kkopka35@gmail.com>
2024-06-22 15:31:42 +0400

    add binary search

--- src/main/java/ru/learning/BinarySearch.java
+++ src/main/java/ru/learning/BinarySearch.java
@@ -1,10 +1,22 @@
 package ru.learning;
 
-import org.testng.annotations.Test;
-
-@Test
 public class BinarySearch {
 
+   public int search(int[]arr, int num) {
+       int low = 0;
+       int high = arr.length - 1;
 
+       while (low <= high) {
+           int mid = (low + high) / 2;
+           if (arr[mid] == num) {
+               return mid;
+           } else if (arr[mid] > num) {
+               high = mid -1;
+           } else {
+               low = mid + 1;
+           }
+       }
+       return -1;
+   }
 
 }
--- src/test/java/ru/learning/binary/search/BinarySearchTests.java
+++ src/test/java/ru/learning/binary/search/BinarySearchTests.java
@@ -1,2 +1,43 @@
-package ru.learning.binary.search;public class BinarySearchTests {
+package ru.learning.binary.search;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import ru.learning.BinarySearch;
+
+import static org.testng.Assert.assertEquals;
+
+@Test
+public class BinarySearchTests {
+
+    private final BinarySearch binarySearch = new BinarySearch();
+
+    @Test(dataProvider = "binarySearchDataProvider")
+    public void binarySearchPosTests(int[] arr, int searchNumber, int expectedIndex) {
+        assertEquals(binarySearch.search(arr, searchNumber), expectedIndex);
+    }
+
+    @Test(dataProvider = "binarySearchNegativeTests")
+    public void binarySearchNegativeTests(int[] arr, int searchNumber, int expectedIndex) {
+        assertEquals(binarySearch.search(arr, searchNumber), expectedIndex);
+    }
+
+    @DataProvider(name = "binarySearchNegativeTests")
+    public Object[][] binarySearchNegativeTests() {
+        return new Object[][]{
+                {new int[]{1, 2, 3, 4, 5}, 10, -1},
+                {new int[]{1, 2, 3, 4, 5}, -10, -1},
+                {new int[]{1, 2, 3, 4, 5}, 0, -1}
+        };
+    }
+
+    @DataProvider(name = "binarySearchDataProvider")
+    public Object[][] binarySearchDataProvider() {
+        return new Object[][]{
+                {new int[]{1, 2, 3, 4, 5}, 5, 4},
+                {new int[]{1, 2, 3, 4, 5}, 1, 0},
+                {new int[]{1, 2, 3, 4, 5}, 3, 2}
+        };
+    }
+
+
 }
